1d96213a21ddf476720cd0686afef8d54de9f1bf
Amulya Awasthi <amulyaawasth18jan@gmail.com>
2026-05-01 23:17:43 +0530

    Modofied app.js file added intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import './App.css';
+
 import Home from "./pages/Home";
 import Admission from "./pages/Admission";
 import About from "./pages/About";
 import Contactus from "./pages/ContactUS";
 import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
 
 function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
+  if (showIntro) {
+    return <Intro onFinish={() => setShowIntro(false)} />;
+  }
+
   return (
     <Router>
       <Navbar />
